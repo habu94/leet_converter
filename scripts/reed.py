7dd96a50a49b8eac40fcb3d871e0f876d5a03dc2
habu94 <s18C1094NG@s.chibakoudai.jp>
2021-01-18 00:04:55 +0900

    Changed

--- scripts/reed.py
+++ scripts/reed.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 import rospy
+from std_msgs.msg import Int32
+
+rospy.init_node('reed')
+pub = rospy.Publisher('reed_data', Int32, queue_size=1)]
+rate = rospy.Rate(10)
+
+while not rospy.is_shutdown():
+    print("Please type a word")
+
+if __name__ == '__main__':
