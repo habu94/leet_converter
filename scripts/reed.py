33a68087b0f57a9943bbb1e617690c72f67f2358
habu94 <s18C1094NG@s.chibakoudai.jp>
2021-01-19 00:39:57 +0900

    Changed

--- scripts/reed.py
+++ scripts/reed.py
@@ -6,8 +6,6 @@ rospy.init_node('reed')
 pub = rospy.Publisher('reed_data', String, queue_size=10)
 rate = rospy.Rate(10)
 
-i = 0
-
 while not rospy.is_shutdown():
     word = input(r"変換したい文字列を入力: ") #文字列を取得
     pub.publish(word)
